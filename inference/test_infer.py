4d2b9d0b1e513d345a1add45edf276f22785a4ae
j999yp <j999yp@163.com>
2022-12-12 13:58:29 +0800

    摄像机上下旋转还有点问题

--- inference/test_infer.py
+++ inference/test_infer.py
@@ -1,29 +1,69 @@
-import numpy as np
-
 import inference
-import cv2 as cv
+import numpy as np
+import cv2
+import matplotlib
 from matplotlib import pyplot as plt
+from math import *
+from scipy.spatial.transform import Rotation
+
+x, y, z = 0, 5, 0.5
+eye_angle = [180, 90]
+
+unit_vec = [1, 0, 0]
+base_r = Rotation.from_euler("YXZ", eye_angle+[0], degrees=True).as_matrix()
+base_vec = np.matmul(unit_vec, base_r)  # 初始法向量
+del base_r, unit_vec
+
+
+def kbd_press(event):
+    global x, y, z, eye_angle
+    r = Rotation.from_euler(
+        "ZXY", [eye_angle[0]-180, 0, 0], degrees=True).as_matrix()
+    up_r = Rotation.from_euler(
+        "YXZ", [eye_angle[1]-90, 0, 0], degrees=True).as_matrix()
+    vec = np.matmul(base_vec, r)
+    up = np.matmul(base_vec, up_r)
+    if (event.key == 'a'):
+        x -= vec[0]
+        y += vec[1]
+    if (event.key == 'd'):
+        x += vec[0]
+        y -= vec[1]
+    if (event.key == 'w'):
+        x += vec[1]
+        y += vec[0]
+    if (event.key == 's'):
+        x -= vec[1]
+        y -= vec[0]
+    if (event.key == ' '):
+        x -= up[1]
+        y -= up[2]
+        z -= up[0]
+    if (event.key == 'control'):
+        x += up[1]
+        y += up[2]
+        z += up[0]
+    if (event.key == 'q'):
+        exit(0)
+    if (event.key == 'right'):
+        eye_angle[0] -= 10
+    if (event.key == 'left'):
+        eye_angle[0] += 10
+    if (event.key == 'up'):
+        eye_angle[1] += 10
+    if (event.key == 'down'):
+        eye_angle[1] -= 10
+
 
 nr = inference.NerfRunner("../models/lego_4/params.pkl")
-x, y, z = 0, -3.5, 3.5
+matplotlib.use("TkAgg")
+plt.rcParams['keymap.save'] = ''
+fig = plt.figure()
+fig.canvas.mpl_connect("key_press_event", kbd_press)
+
 while (True):
-    img = nr.inference([x, y, z], [45, 0, 0])
-    cv.imshow("", img)
-    key = cv.waitKey(0)
-    if (chr(key) == 'a'):
-        x -= 0.5
-    elif (chr(key) == 'd'):
-        x += 0.5
-    elif (chr(key) == 'w'):
-        y += 0.5
-    elif (chr(key) == 's'):
-        y -= 0.5
-    elif (chr(key) == 'z'):
-        z -= 0.5
-    elif (chr(key) == 'c'):
-        z += 0.5
-    elif (chr(key) == 'q'):
-        break
-    #img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-    #cv.imwrite("./temp/test.png", img)
-    #print("rendered image is saved.")
+    img = nr.inference([x, y, z], [eye_angle[1], eye_angle[0], 0], "YXZ")
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.show(block=False)
+    print("pos:", [x, y, z], "dir", eye_angle)
+    plt.waitforbuttonpress(10)
